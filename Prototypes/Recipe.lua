c0f30b09207711b22ff304ed4a5f7c7dc006c729
Brent-Call <tnerbcall@comcast.net>
2023-02-17 19:21:18 -0500

    Make recipes the same in normal & expensive mode

--- Prototypes/Recipe.lua
+++ Prototypes/Recipe.lua
@@ -2,89 +2,39 @@ data:extend({
 {
 	type = "recipe",
 	name = "Q-InserterModule:inserter-module",
-	--Normal recipe difficulty:
-	normal =
+	enabled = false,
+	energy_required = 15,
+	ingredients = 
 	{
-		enabled = false,
-		energy_required = 15,
-		ingredients = 
-		{
-			{ "electronic-circuit", 5 },
-			{ "advanced-circuit", 5 }
-		},
-		result = "Q-InserterModule:inserter-module"
+		{ "electronic-circuit", 5 },
+		{ "advanced-circuit", 5 }
 	},
-	--Expensive recipe difficulty:
-	expensive =
-	{
-		enabled = false,
-		energy_required = 15,
-		ingredients = 
-		{
-			{ "electronic-circuit", 10 },
-			{ "advanced-circuit", 10 }
-	    	},
-		result = "Q-InserterModule:inserter-module"
-	}
+	result = "Q-InserterModule:inserter-module"
 },
 {
 	type = "recipe",
 	name = "Q-InserterModule:inserter-module-2",
-	--Normal recipe difficulty:
-	normal =
+	enabled = false,
+	energy_required = 30,
+	ingredients = 
 	{
-		enabled = false,
-		energy_required = 30,
-		ingredients = 
-		{
-			{ "Q-InserterModule:inserter-module", 2 },
-			{ "advanced-circuit", 5 },
-			{ "processing-unit", 5 }
-		},
-		result = "Q-InserterModule:inserter-module-2"
+		{ "Q-InserterModule:inserter-module", 2 },
+		{ "advanced-circuit", 5 },
+		{ "processing-unit", 5 }
 	},
-	--Expensive recipe difficulty:
-	expensive =
-	{
-		enabled = false,
-		energy_required = 30,
-		ingredients = 
-		{
-			{ "Q-InserterModule:inserter-module", 2 },
-			{ "advanced-circuit", 10 },
-			{ "processing-unit", 10 }
-		},
-		result = "Q-InserterModule:inserter-module-2"
-	}
+	result = "Q-InserterModule:inserter-module-2"
 },
 {
 	type = "recipe",
 	name = "Q-InserterModule:inserter-module-3",
-	--Normal recipe difficulty:
-	normal =
+	enabled = false,
+	energy_required = 60,
+	ingredients = 
 	{
-		enabled = false,
-		energy_required = 60,
-		ingredients = 
-		{
-      		{ "Q-InserterModule:inserter-module-2", 2 },
-      		{ "effectivity-module-2", 1 },
-      		{ "processing-unit", 5 }
-		},
-		result = "Q-InserterModule:inserter-module-3"
+		{ "Q-InserterModule:inserter-module-2", 2 },
+		{ "effectivity-module-2", 1 },
+		{ "processing-unit", 5 }
 	},
-	--Expensive recipe difficulty:
-	expensive =
-	{
-		enabled = false,
-		energy_required = 60,
-		ingredients = 
-		{
-			{ "Q-InserterModule:inserter-module-2", 2 },
-			{ "effectivity-module-2", 1 },
-			{ "processing-unit", 10 }
-    		},
-		result = "Q-InserterModule:inserter-module-3"
-	}
+	result = "Q-InserterModule:inserter-module-3"
 }
 })
